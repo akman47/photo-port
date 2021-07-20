23415481d88f1899f9d737a2e1198d7073b48fb0
Angela Man <angela.k.man@gmail.com>
2021-07-19 23:28:42 -0500

    initialized and set control state for contact form

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,21 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function ContactForm() {
+    // initializing state
+    const [formState, setFormState] = useState({ name:'', email:'', message:'' });
+    const {name, email, message} = formState;
+
+    // sync internal state of component formState with user input from DOM
+    function handleChange(e) {
+        setFormState({...formState, [e.target.name]: e.target.value });
+    }
+
+    //console.log(formState);
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(formState);
+    }
+
     return (
         <section>
             <h1>Contact me</h1>
-            <form id="contact-form">
+            <form id="contact-form" onSubmit={handleSubmit}>
                 <div>
                     <label htmlFor="name">Name:</label>
-                    <input type="text" name="name" />
+                    <input type="text" name="name" defaultValue={name} onChange={handleChange} />
                 </div>
                 <div>
                     <label htmlFor="email">Email address:</label>
-                    <input type="text" name="email" />
+                    <input type="text" name="email" defaultValue={email} onChange={handleChange} />
                 </div>
                 <div>
                     <label htmlFor="message">Message:</label>
-                    <textarea name="message" rows="5" />
+                    <textarea name="message" rows="5" defaultValue={message} onChange={handleChange} />
                 </div>
                 <button type="submit">Submit</button>
             </form>
